9fb65c4236102a868090a6c6e0e8e675b06f1ab7
Brad Heller <brad.heller@gmail.com>
2023-09-27 14:12:25 +0000

    Add unit tests for buffer.

---
+++ bytesx/buffer_test.go
@@ -0,0 +1,44 @@
+package bytesx
+
+import (
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuffer(t *testing.T) {
+	t.Run("can write and read from buffer", func(t *testing.T) {
+		buf := NewBuffer([]byte{})
+
+		t.Run("can write to buffer", func(t *testing.T) {
+			n, err := buf.Write([]byte("Hello, world!"))
+			assert.NoError(t, err)
+			assert.Equal(t, n, 13)
+		})
+
+		t.Run("can rewind buffer", func(t *testing.T) {
+			n, err := buf.Seek(0, io.SeekStart)
+			assert.NoError(t, err)
+			assert.Equal(t, n, int64(0))
+		})
+
+		t.Run("can read from buffer", func(t *testing.T) {
+			var b [5]byte
+			n, err := buf.Read(b[:])
+			assert.NoError(t, err)
+			assert.Equal(t, n, 5)
+			assert.Equal(t, string(b[:]), "Hello")
+		})
+	})
+
+	t.Run("can read from a seeded buffer", func(t *testing.T) {
+		buf := NewBuffer([]byte("Hello, world!"))
+
+		var b [5]byte
+		n, err := buf.Read(b[:])
+		assert.NoError(t, err)
+		assert.Equal(t, n, 5)
+		assert.Equal(t, string(b[:]), "Hello")
+	})
+}
